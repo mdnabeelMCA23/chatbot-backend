dcf2788a9b7d71fb59a828dbf36383aa56b82fe2
Mohammed Nabeel P <111896737+mdnabeelMCA23@users.noreply.github.com>
2025-11-22 11:53:35 +0530

    Update chatController.js

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -14,7 +14,7 @@ exports.chatWithBot = async (req, res) => {
 
   try {
     const botResponse = await axios.post('https://openrouter.ai/api/v1/chat/completions', {
-      model: "nvidia/nemotron-nano-12b-v2-vl:free",
+      model: "x-ai/grok-4.1-fast:free",
       messages: [{ role: "user", content: message }]
     }, {
       headers: {
